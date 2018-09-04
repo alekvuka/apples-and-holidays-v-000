9da1b9fcdccd838dc59f6c8f2b727dd186cd31b4
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-09-04 22:32:55 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -104,6 +104,7 @@ def all_supplies_in_holidays(holiday_hash)
       puts "Winter:"
       holidays.each do |holiday, supplies|
         holiday = holiday.to_s
+        holiday = holiday.gsub("_", " ") 
         holiday = holiday.gsub(/\w+/, &:capitalize)
         puts "  #{holiday}: #{supplies.join(", ")}"
 
